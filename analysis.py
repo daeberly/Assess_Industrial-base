74bba04f24236468cf456967fed8c6a8895383bd
daeberly <68342740+daeberly@users.noreply.github.com>
2021-06-16 17:46:02 -0400

    added analysis & broke up monthly/qtr

--- analysis.py
+++ analysis.py
@@ -14,91 +14,416 @@ Created on Mon Jun 14 14:33:38 2021
 
 import pandas as pd
 import numpy as np
-import time
-import datetime
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-start = time.time()
+# Companies of interest
+companies = ['LMT', 'RTX', 'BA' , 'GD', 'GE', 'HII', 'LHX']
+
+#%%
 
 # import pickle file
-measures=pd.read_pickle("measures.pkl.zip")
+measures=pd.read_pickle("measures_qtrly.pkl.zip")
+stock_info=pd.read_pickle("stock_info.pkl.zip")
 
-# Create ROE, ROA, ROI Variables
+#%%
+
+# Create ROE and ROA Variables
 measures["ROA"] = measures["NetIncome"]/measures["TotalAssets"]
 measures["ROE"] = measures["NetIncome"]/measures["StockholdersEquity"]
-print(measures["ROA"].value_counts())
 
-# Duplicate problem
-print(measures.iloc[[311]])
-print(measures.iloc[[270]])
-
-# isolate last ten years
-start = pd.to_datetime('2011-03-31 00:00:00')
-end = pd.to_datetime('2021-03-31 00:00:00')
-
-measures_ten = measures.loc[(('index < @start or index > @end')                            ​
-
+#%%
 #
-# should this go in financials.py?
+# isolate time frames
 #
 
-# import R&D data 
-RD_data = pd.read_csv('R&D_data.csv')
+financials_10yr = measures.query("date > '2010-01-29 00:00:00' and date < '2021-06-11 00:00:00'")
+financials_5yr = measures.query("date > '2016-01-29 00:00:00' and date < '2021-06-11 00:00:00'")
+financials_3yr = measures.query("date > '2018-01-29 00:00:00' and date < '2021-06-11 00:00:00'")
+financials_1yr = measures.query("date > '2020-01-29 00:00:00' and date < '2021-06-11 00:00:00'")
+
+stocks_10yr = stock_info.query("Date > '2010-01-29 00:00:00' and Date < '2021-06-11 00:00:00'")
+stocks_5yr = stock_info.query("Date > '2016-01-29 00:00:00' and Date < '2021-06-11 00:00:00'")
+stocks_3yr = stock_info.query("Date > '2018-01-29 00:00:00' and Date < '2021-06-11 00:00:00'")
+stocks_1yr = stock_info.query("Date > '2020-01-29 00:00:00' and Date < '2021-06-11 00:00:00'")
+
+#%%
+
+##
+## Accounts Payable Turnover Ratios
+##
+
+# To help w/calculations... created smaller dataframe to drop NaN
+ 
+AP = financials_10yr[['CostOfRevenue','AccountsPayable']]
+print ( AP )
+
+# Keep only records with data
+AP = AP.dropna()
+print( AP )
+
+#
+# Calculate Ratios with 'for' loop
+#
+
+# Used loop because I couldn't figure out how to do it with groupby()
+
+
+apt = pd.DataFrame()
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = AP.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+   
+    # reset index because of trouble doing .diff() calc w/ 'date' in the index
+    temp = temp.reset_index()
+    temp['days'] = temp['date'].diff()
+        # convert nanoseconds to days
+    temp['days'] = temp['days'].astype('timedelta64[D]')
+  
+    ## Accounts Payable Turnover Ratio
+
+    # calculate average for AccountsPayable (column #3) 
+        # current & previous row is window=2  
+    temp['APT_period_ave'] =  temp.iloc[:,3].rolling(window=2).mean()
+
+    #  calculate ratio
+    temp['APT'] = temp['CostOfRevenue'] / temp['APT_period_ave']
+
+    # reset index to date
+    temp = temp.set_index('date')
+  
+    ## Accounts Payable Turnover Ratio DAYS
+ 
+    temp['APT_days'] = temp['days'] / temp['APT'] 
+
+    apt = apt.append( temp )
+    
+print ( apt )
+
+#%%
+
+##
+## Import & format R&D data
+##
+
+data_type = {'filing': str, 'ticker':str}
+RD_data = pd.read_csv('R&D_data.csv', dtype = data_type)
+
+#  Format data types
+RD_data.dtypes
+RD_data['Date'] = pd.to_datetime(RD_data['Date'])   # keep only the date, remove hr/min/sec
+RD_data['Date'] = RD_data['Date'].dt.date   # keep only the date, remove hr/min/sec
 RD_data.dtypes
 
-    # set datetime format
-date_type = {'Date': 'datetime64','ticker':str, 'filing':str}
-RD_data = RD_data.astype(date_type)
-RD_data['Date']= RD_data['Date'].dt.date
-
-    # merge RD_data w/ measures by date & ticker
-
-
-
 #%%
 
-
-#
-# Calculate ratios
-# 
-
-
-
-#
-# Accounts Payable Turnover 
-#
-    # number of times debt can be paid off per period
-
-# APT_ratio = 'CostOfRevenue' / (average of 'AccountsPayable' current & previous quarter)
-
-
-#
-# Accounts Payable Turnover Days
-# 
-   # how many days to payoff debts
-
-# APT_days_ratio = days in period(date current - date previous) / APT Ratio
-
-# take difference first
-# look up dateteime time delta object (days)
-
-    # calculate number of days between periods
-#sample[‘t_val’] = sample.index
-#sample[‘delta’] = (sample[‘t_val’]-sample[‘t_val’].shift()).fillna(0)
-
-
-#
-# Normalize data for plotting
-#
-
+##
+## Stock - Normalize Stock prices & calculate ROI 
+##
 
 #%%
 
 #
-# Plots...
+#  Stock Calcs - 10 YR 
 #
 
-    # plot measures w/horizontal or vertical reference lines
-    # model - forecast
-    # heatmap - correlation factors
+# Normalize stock price plus ROI
+stock_info_10yr = pd.DataFrame()
+
+# To collect final ROI
+finalROIs = pd.DataFrame()
+    # My vision for 'finalROIs' is:
+            #           10yrROI     10yrROI_including Dividends .... 5yrROI
+            # Ticker
+            # LMT
+            # BA
+            # ...
+            # GE
+print('If you purchased 1 stock in 2/2011 & sold it in 6/2021.')
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = stocks_10yr.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+    
+    # sort data  
+    temp = temp.sort_index( ascending= False )
+    # select last row
+    start_date = temp.iloc[-1]
+    # normalize stock based on start date
+    temp['norm_close'] = temp['Close']/start_date['Close']
+
+    # calculate ROI with $100 dollar investment
+    temp['ROI'] = 100* (( temp['Close'] - start_date['Close'] ) / start_date['Close'] )
+    
+    stock_info_10yr = stock_info_10yr.append( temp )
+    
+    # total ROI plus dividends
+    end_date = temp.iloc[0]
+    total_ROI = 100* (( end_date['Close'] - start_date['Close'] + temp['Dividends'].sum()) / start_date['Close'] )
+    
+    print('Stock:',tick,
+          '| ROI:', round(end_date['ROI']),
+          '%| ROI plus dividends', round(total_ROI),"%")
+    #finalROI_10yr = finalROI_10yr.append( total_ROI )
+    
+#print( stock_info_10yr )
+
+#%%
+#
+#  Stock Calcs - 5 YR 
+#
+
+# Normalize stock price plus ROI
+stock_info_5yr = pd.DataFrame()
+
+print('If you purchased 1 stock in 2/2016 & sold it in 6/2021.')
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = stocks_5yr.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+    
+    # sort data  
+    temp = temp.sort_index( ascending= False )
+    # select last row
+    start_date = temp.iloc[-1]
+    # normalize stock based on start date
+    temp['norm_close'] = temp['Close']/start_date['Close']
+
+    # calculate ROI with $100 dollar investment
+    temp['ROI'] = 100* (( temp['Close'] - start_date['Close'] ) / start_date['Close'] )
+    
+    stock_info_5yr = stock_info_5yr.append( temp )
+    
+    # total ROI plus dividends
+    end_date = temp.iloc[0]
+    total_ROI = 100* (( end_date['Close'] - start_date['Close'] + temp['Dividends'].sum()) / start_date['Close'] )
+    
+    print('Stock:',tick,
+          '| ROI:', round(end_date['ROI']),
+          '%| ROI plus dividends', round(total_ROI),"%")
+
+#print( stock_info_5yr )
+
+#%%
+#
+#  Stock Calcs - 3 YR 
+#
+
+# Normalize stock price plus ROI
+stock_info_3yr = pd.DataFrame()
+
+print('If you purchased 1 stock in 2/2018 & sold it in 6/2021.')
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = stocks_3yr.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+    
+    # sort data  
+    temp = temp.sort_index( ascending= False )
+    # select last row
+    start_date = temp.iloc[-1]
+    # normalize stock based on start date
+    temp['norm_close'] = temp['Close']/start_date['Close']
+
+    # calculate ROI with $100 dollar investment
+    temp['ROI'] = 100* (( temp['Close'] - start_date['Close'] ) / start_date['Close'] )
+    
+    stock_info_3yr = stock_info_3yr.append( temp )
+    
+    # total ROI plus dividends
+    end_date = temp.iloc[0]
+    total_ROI = 100* (( end_date['Close'] - start_date['Close'] + temp['Dividends'].sum()) / start_date['Close'] )
+    
+    print('Stock:',tick,
+          '| ROI:', round(end_date['ROI']),
+          '%| ROI plus dividends', round(total_ROI),"%")
+
+#print( stock_info_5yr )
+
+#%%
+#
+#  Stock Calcs - 1 YR 
+#
+
+# Normalize stock price plus ROI
+stock_info_1yr = pd.DataFrame()
+
+print('If you purchased 1 stock in 2/2020 & sold it in 6/2021.')
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = stocks_1yr.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+    
+    # sort data  
+    temp = temp.sort_index( ascending= False )
+    # select last row
+    start_date = temp.iloc[-1]
+    # normalize stock based on start date
+    temp['norm_close'] = temp['Close']/start_date['Close']
+
+    # calculate ROI with $100 dollar investment
+    temp['ROI'] = 100* (( temp['Close'] - start_date['Close'] ) / start_date['Close'] )
+    
+    stock_info_1yr = stock_info_1yr.append( temp )
+    
+    # total ROI plus dividends
+    end_date = temp.iloc[0]
+    total_ROI = 100* (( end_date['Close'] - start_date['Close'] + temp['Dividends'].sum()) / start_date['Close'] )
+    
+    print('Stock:',tick,
+          '| ROI:', round(end_date['ROI']),
+          '%| ROI plus dividends', round(total_ROI),"%")
+
+#print( stock_info_5yr )
+
+
+#%%
+
+#
+# ROA & ROE
+#
+
+#%%
+
+#
+# 10 yr
+#
+
+norm = financials_10yr[['ROA','ROE']]
+
+# Keep only records with data
+norm = norm.dropna()
+
+# Normalize ROA and ROE based on first quarter 
+norm_fin_10yr = pd.DataFrame()
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = norm.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+    
+    # normalize 
+    temp1 = temp.xs('2011-03-31 00:00:00')
+    temp['norm_ROA'] = temp['ROA']/temp1['ROA']
+    temp['norm_ROE'] = temp['ROE']/temp1['ROE']
+    
+    norm_fin_10yr = norm_fin_10yr.append( temp )
+    
+print ( norm_fin_10yr )
+
+
+#%%
+
+#
+# 5 yr
+#
+
+norm = financials_5yr[['ROA','ROE']]
+
+# Keep only records with data
+norm = norm.dropna()
+
+# Normalize ROA and ROE based on first quarter
+norm_fin_5yr = pd.DataFrame()
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = norm.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+    
+    # normalize 
+    temp1 = temp.xs('2016-03-31 00:00:00')
+    temp['norm_ROA'] = temp['ROA']/temp1['ROA']
+    temp['norm_ROE'] = temp['ROE']/temp1['ROE']
+    
+    
+    norm_fin_5yr = norm_fin_5yr.append( temp )
+    
+print ( norm_fin_5yr )
+
+#%%
+#
+# 10 yr
+#
+
+norm = financials_3yr[['ROA','ROE']]
+
+# Keep only records with data
+norm = norm.dropna()
+
+# Normalize ROA and ROE based on first quarter
+norm_fin_3yr = pd.DataFrame()
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = norm.query("ticker == @tick")
+    temp = temp.copy()  # .copy() needed fix Pandas Error when calculating
+    
+    # normalize 
+    temp1 = temp.xs('2018-03-31 00:00:00')
+    temp['norm_ROA'] = temp['ROA']/temp1['ROA']
+    temp['norm_ROE'] = temp['ROE']/temp1['ROE']
+    
+    norm_fin_3yr = norm_fin_3yr.append( temp )
+    
+print ( norm_fin_3yr )
+#%%
+#
+# 10 yr
+#
+
+norm = financials_1yr[['ROA','ROE']]
+
+# Keep only records with data
+norm = norm.dropna()
+
+# Normalize ROA and ROE based on first quarter
+norm_fin_1yr = pd.DataFrame()
+
+for ticker in companies:
+  
+    tick = ticker
+    temp = norm.query("ticker == @tick")
+    temp = temp.copy()
+    
+    # normalize 
+    temp1 = temp.xs('2020-03-31 00:00:00')
+    temp['norm_ROA'] = temp['ROA']/temp1['ROA']
+    temp['norm_ROE'] = temp['ROE']/temp1['ROE']
+    
+    norm_fin_1yr = norm_fin_1yr.append( temp )
+    
+print ( norm_fin_1yr )
+#%%
+#Pickle dfs for plotting in another file
+
+norm_fin_1yr.to_pickle("norm_fin_1yr.pkl")
+norm_fin_3yr.to_pickle("norm_fin_3yr.pkl")
+norm_fin_5yr.to_pickle("norm_fin_5yr.pkl")
+norm_fin_10yr.to_pickle("norm_fin_10yr.pkl")
+
+financials_1yr.to_pickle("financials_1yr.pkl")
+financials_3yr.to_pickle("financials_3yr.pkl")
+financials_5yr.to_pickle("financials_5yr.pkl")
+financials_10yr.to_pickle("financials_10yr.pkl")
+
+stock_info_1yr.to_pickle("stocks_1yr.pkl")
+stock_info_3yr.to_pickle("stocks_3yr.pkl")
+stock_info_5yr.to_pickle("stocks_5yr.pkl")
+stock_info_10yr.to_pickle("stocks_10yr.pkl")
+
+apt.to_pickle('apt.pkl')
+
+RD_data.to_pickle('RD_data.pkl')
--- financials.py
+++ financials.py
@@ -14,12 +14,14 @@ Created on Sun Jun 13 18:04:46 2021
 # .csv reports downloaded on 6.13.2021
 
 
-# This code creates...
+# File has 3 sections that produce:
 
-    # 'DIB_Financials' dataframe (2021-1985) includes:
+    #1 Quarterly - 'DIB_Financials' dataframe (2021-1985) includes:
         # fundamentals, cash flow, balance sheet & valuation measures
-        
-    # 'stock_info' dataframe (2021 ~ 1980s) includes:
+     
+    #2 Monthly - 'DIB_Financials' dataframe (2021-1985) includes:
+
+    #3 'stock_info' dataframe (2021 ~ 1980s) includes:
         # open, high, low, close, volume, dividend per stock, stock split
 
 
@@ -35,7 +37,7 @@ current = datetime.date.today()
 #%%
 
 
-# Combined Financials Sheets (Years 2021-1985)
+# QUARTERLY Financials Sheets (Years 2021-1985)
 
 
 #
@@ -90,7 +92,7 @@ for file in glob.glob(path, recursive = False):
 
     stack_all = stack_all.append(stk)
 
-#%%
+
 #
 #  Show the result for checking
 #
@@ -121,14 +123,10 @@ uns = uns.drop('ttm',axis=0)
 uns = uns.reset_index()
 uns['date'] = pd.to_datetime( uns['date'] )
 uns['date'] = uns['date'].dt.date    # keep only the date, remove hr/min/sec
-uns = uns.set_index(['date'])
+uns = uns.set_index(['date','ticker'])
 DIB_Financials = uns.sort_index( ascending= False)
 
 print ( DIB_Financials )
-
-
-
-#%%
 
 #
 # Trim dataframe to only financial measures of interest
@@ -142,16 +140,141 @@ keep = pd.read_csv('measures_toKeep.csv')
 keep = keep.Measures.to_list()
 measures = DIB_Financials[ keep ]
 
+#
 # export to .pkl file w/ zip compression added
-measures.to_pickle("measures.pkl.zip")
+#
+measures.to_pickle("measures_qtrly.pkl.zip")
 
-# save a sample
-sample = measures.sample(1000)
-sample.to_csv(str(current) + '_DIB_financials.csv')
+print('\n Financials exported to: measures_qrtly.pkl.zip')
 
-print('\n Financials exported to: measures.pkl.zip')
+'''
+# sample
+sample = measures.sample(frac = 0.1)
+measures.to_csv(str(current) + '_sample_measures_qrtly.csv')
+'''
 
 #%%
+
+
+# MONTHLY Financials Sheets (Years 2021-1985)
+
+
+#
+#  Blank dataframe to hold the accumulated data
+#
+
+stack_all = pd.DataFrame()
+
+#
+#  Read the files one by one and stack the data onto stack_all
+#
+path = "raw_monthly/*.csv"
+
+for file in glob.glob(path, recursive = False):
+
+    raw = pd.read_csv(file,dtype=str)
+    
+    #  remove spaces and use the name as the index
+    
+    raw['name'] = raw['name'].str.strip()
+    raw = raw.set_index('name')
+
+    #  stack all the remaining columns; the date will become a level 
+    #  in the index
+    
+    stk = raw.stack()
+    
+    #  set the names of the resulting index levels and the name of 
+    #  the remaining data values
+    
+    stk.index.names = ['name','date']
+    stk.name = 'value'
+    
+    #  remove commas and convert the data to floats
+    
+    stk = stk.str.replace(',','').astype(float)
+    
+    #  look for name,date duplicates
+    
+    dups = stk.index.duplicated()
+    print( f'Duplicates in {file}:', dups.sum() )
+    
+    # convert Series to Dataframe
+    stk = stk.to_frame()
+        
+    # add ticker to each row in data
+    ticker = os.path.splitext(os.path.basename(file))[0]
+    ticker = ticker.split('_')[0]     
+    stk ['ticker'] = ticker
+    
+    #  append the data to the main stack
+
+    stack_all = stack_all.append(stk)
+
+
+#
+#  Show the result for checking
+#
+
+print( stack_all )
+
+#
+#  Add ticker to index prior in order to unstack
+#
+
+stack_all = stack_all.set_index(['ticker'], append=True)
+
+print( stack_all )
+
+#
+#  Turn the names into columns
+#
+
+uns = stack_all.unstack('name')['value']
+
+print( uns )
+
+#
+#  Get rid of TTM, convert dates, and sort
+#
+
+uns = uns.drop('ttm',axis=0)
+uns = uns.reset_index()
+uns['date'] = pd.to_datetime( uns['date'] )
+uns['date'] = uns['date'].dt.date    # keep only the date, remove hr/min/sec
+uns = uns.set_index(['date','ticker'])
+DIB_Financials = uns.sort_index( ascending= False)
+
+print ( DIB_Financials )
+
+#
+# Trim dataframe to only financial measures of interest
+#
+
+
+# import .csv with measures of interest
+keep = pd.read_csv('measures_toKeep_monthly.csv')
+
+# create a new dataframe with only those measures
+keep = keep.Measures.to_list()
+measures = DIB_Financials[ keep ]
+
+#
+# export to .pkl file w/ zip compression added
+#
+measures.to_pickle("measures_monthly.pkl.zip")
+
+print('\n Financials exported to: measures_monthly.pkl.zip')
+
+'''
+# sample
+sample = measures.sample(frac = 0.1)
+measures.to_csv(str(current) + '_sample_measures_monthly.csv')
+'''
+
+
+#%%
+
 # 
 # Stock info (2021 to ~1980s)
 #
@@ -185,18 +308,23 @@ stock_info = stock_info.set_index(['Date', 'ticker'])
 # export to .pkl
 stock_info.to_pickle('stock_info.pkl.zip')
 
-print('\n Stock prices exported to : stock_info.pkl.zip')
+print('\nStock prices exported to : stock_info.pkl.zip')
 
-
+'''
 # sample
 sample = stock_info.sample(1000)
 sample.to_csv(str(current) + '_sample_stock_info.csv')
-    
+'''
+
+  
 #%%
 
 #
 # Housekeeping
 #
+print('Financials exported to: measures_monthly.pkl.zip')
+print('Financials exported to: measures_qrtly.pkl.zip')
+
 
 end = time.time()
 print('\nPart 1 = Great Success! \nTotal Processing Time:', round(end-start,2), 'seconds\n')
